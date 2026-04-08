f9fb6c6e98afef7329654ac7a662cde099843775
7savv7 <schrysostomid@gmail.com>
2026-04-09 02:28:55 +0300

    Sign in page

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,9 +1,11 @@
-import "./../../globals.css"
 import { Text, View } from "react-native";
+import "./../../globals.css"
+import {Link} from "expo-router";
 
 export default function App() {
     return (
         <View className="flex-1 items-center justify-center bg-white">
+            <Link href={"/sign-in"}>Sign In</Link>
             <Text className="text-xl text-blue-500 font-rubik">
                 Welcome to Nativewind!
             </Text>
